b4103e5f3dff4cf1eda168e32d0d068d94372cf7
hsmith917 <89433847+hsmith917@users.noreply.github.com>
2024-04-27 18:24:05 -0400

    some work on parsetree.h

--- parse_tree.h
+++ parse_tree.h
@@ -1,1 +1,114 @@
 // Code file containing class definitions and code to respond to the statements in the code.
+#include <map>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+//integer expressions
+class integer_expression {
+ public:
+  virtual int evaluate_expression(map<string, int> &sym_tab) =0;
+  
+};
+
+class plus_expr: public integer_expression {
+ public:
+  plus_expr(integer_expression *left, integer_expression *right) {
+    l = left;
+    r = right;
+  }
+
+  virtual int evaluate_expression(map<string, int> &sym_tab) {
+    return l->evaluate_expression(sym_tab) + r->evaluate_expression(sym_tab);
+  }
+  
+  private:
+    integer_expression *l;
+    integer_expression *r;
+};
+
+class minus_expr: public integer_expression {
+ public:
+  minus_expr(integer_expression *left, integer_expression *right) {
+    l = left;
+    r = right;
+  }
+
+  virtual int evaluate_expression(map<string, int> &sym_tab) {
+    return l->evaluate_expression(sym_tab) - r->evaluate_expression(sym_tab);
+  }
+  
+  private:
+    integer_expression *l;
+    integer_expression *r;
+};
+
+class mult_expr: public integer_expression {
+ public:
+  mult_expr(integer_expression *left, integer_expression *right) {
+    l = left;
+    r = right;
+  }
+
+  virtual int evaluate_expression(map<string, int> &sym_tab) {
+    return l->evaluate_expression(sym_tab) * r->evaluate_expression(sym_tab);
+  }
+  
+  private:
+    integer_expression *l;
+    integer_expression *r;
+};
+
+class div_expr: public integer_expression {
+ public:
+  div_expr(integer_expression *left, integer_expression *right) {
+    l = left;
+    r = right;
+  }
+
+  virtual int evaluate_expression(map<string, int> &sym_tab) {
+    return l->evaluate_expression(sym_tab) / r->evaluate_expression(sym_tab);
+  }
+  
+  private:
+    integer_expression *l;
+    integer_expression *r;
+};
+
+class mod_expr: public integer_expression {
+ public:
+  mod_expr(integer_expression *left, integer_expression *right) {
+    l = left;
+    r = right;
+  }
+
+  virtual int evaluate_expression(map<string, int> &sym_tab) {
+    return l->evaluate_expression(sym_tab) % r->evaluate_expression(sym_tab);
+  }
+  
+  private:
+    integer_expression *l;
+    integer_expression *r;
+};
+
+// string expression
+class string_expression {
+    public:
+        virtual string evaluate_expression(map<string, string> &sym_tab)=0;
+};
+
+class concatenate_expr : public string_expression {
+    public:
+        concatenate_expr(string_expression* left, string_expression* right) : l(left), r(right) {}
+
+        virtual string evaluate_expression(map<string, string>& sym_tab) {
+            return l -> evaluate_expression(sym_tab) + r -> evaluate_expression(sym_tab);
+        }
+
+    private:
+        string_expression* l;
+        string_expression* r;
+};
--- tree_builder.y
+++ tree_builder.y
@@ -22,9 +22,6 @@ extern int yylval;
 %token TKBUILDNODE FOR IN TKNAME TKWEIGHT TKISCHILDOF
 %token <string_literal> TKSTRINGLITERAL 
 %token <int_literal> TKINTLITERAL
-%token SEMICOLON COLON
-%token COMMA PLUS EQUALS
-%token L_BRACE R_BRACE
 %token IDENTIFIER
 
 
@@ -43,23 +40,23 @@ statement : build_node_statement
           | loop_statement
           ;
 
-build_node_statement : TKBUILDNODE L_BRACE attribute_list R_BRACE SEMICOLON
+build_node_statement : TKBUILDNODE '{' attribute_list '}' ';'
                      ;
 
 attribute_list : attribute
                | attribute_list attribute
                ;
 
-attribute : TKNAME EQUALS TKSTRINGLITERAL SEMICOLON
-          | TKWEIGHT EQUALS TKINTLITERAL SEMICOLON
-          | TKISCHILDOF EQUALS TKSTRINGLITERAL SEMICOLON
+attribute : TKNAME '=' TKSTRINGLITERAL ';'
+          | TKWEIGHT '=' TKINTLITERAL ';'
+          | TKISCHILDOF '=' TKSTRINGLITERAL ';'
           ;
 
-loop_statement : FOR IDENTIFIER IN L_BRACE range R_BRACE L_BRACE statement_list R_BRACE
+loop_statement : FOR IDENTIFIER IN '{' range '}' '{' statement_list '}'
                ;
 
-range : TKINTLITERAL COLON TKINTLITERAL
-      | TKSTRINGLITERAL COMMA range
+range : TKINTLITERAL ':' TKINTLITERAL
+      | TKSTRINGLITERAL ',' range
       ;
 
 %%
