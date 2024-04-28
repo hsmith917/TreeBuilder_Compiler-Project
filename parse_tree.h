b978290f955e5c4638ce4bd6c6243b4399e25d3f
hsmith917 <89433847+hsmith917@users.noreply.github.com>
2024-04-28 15:15:09 -0400

    trying anything
    some work done on parse_tree and tree_builder

--- parse_tree.h
+++ parse_tree.h
@@ -1,227 +1,188 @@
-//*****************************************************************************************
-// Filename: parse_tree.h
-// Authors: Hunter Smith and Johnathan Reilly
-// ****************************************************************************************
+#ifndef PARSE_TREE_H
+#define PARSE_TREE_H
+
 #include <map>
 #include <string>
-#include <set>
-#include <algorithm>
-#include <iostream>
-#include <cstddef>
 #include "tree_node.h"
 
 using namespace std;
 
-//integer expressions
 class integer_expression {
- public:
-  virtual int evaluate_expression(map<string, node> &sym_tab) =0;
-  
+public:
+    virtual int evaluate_expression(map<string, node>& sym_tab) = 0;
+    virtual ~integer_expression() {}
 };
 
-class int_constant:public integer_expression {
- public:
-  int_constant(int val) {saved_val = val;}
+class int_constant : public integer_expression {
+public:
+    int_constant(int val) : saved_val(val) {}
 
-  virtual int evaluate_expression(map<string, node> &sym_tab) {
-    return saved_val;
-  }
- private:
-  int saved_val;
-};
-
-class plus_expr: public integer_expression {
- public:
-  plus_expr(integer_expression *left, integer_expression *right) {
-    l = left;
-    r = right;
-  }
-
-  virtual int evaluate_expression(map<string, node> &sym_tab) {
-    return l->evaluate_expression(sym_tab) + r->evaluate_expression(sym_tab);
-  }
-  
-  private:
-    integer_expression *l;
-    integer_expression *r;
-};
-
-class variable: public integer_expression {
- public:
-  variable(char *in_val) {cout << "Found variable = " << in_val << endl; 
-                          saved_val =in_val;}
-
-  virtual int evaluate_expression(map<string, node> &sym_tab) {
-
-    map<string,node>::iterator p;
-    p =sym_tab.find(saved_val);
-    cout << "Looking up variable " << saved_val << endl;
-    if (p!=sym_tab.end()) {
-      cout << "Returning value of variable " << saved_val << endl;
-      cout << "= " << p->second.weight << endl;
-
-      return p->second.weight;
-    } else {
-      // ERROR .... for now return -1;
-      // Should throw error!
-      return -1;
+    virtual int evaluate_expression(map<string, node>& sym_tab) {
+        return saved_val;
     }
 
-  }
- private:
-  string saved_val;
-  
+private:
+    int saved_val;
 };
 
-// string expression
+class plus_expr : public integer_expression {
+public:
+    plus_expr(integer_expression* left, integer_expression* right) : l(left), r(right) {}
+
+    virtual int evaluate_expression(map<string, node>& sym_tab) {
+        return l->evaluate_expression(sym_tab) + r->evaluate_expression(sym_tab);
+    }
+
+private:
+    integer_expression* l;
+    integer_expression* r;
+};
+
+class variable : public integer_expression {
+public:
+    variable(const string& in_val) : saved_val(in_val) {}
+
+    virtual int evaluate_expression(map<string, node>& sym_tab) {
+        auto it = sym_tab.find(saved_val);
+        if (it != sym_tab.end()) {
+            return it->second.weight;
+        } else {
+            cerr << "Variable " << saved_val << " not found!" << endl;
+            return -1; // Error: Variable not found
+        }
+    }
+
+private:
+    string saved_val;
+};
+
 class string_expression {
-    public:
-        virtual string evaluate_expression(map<string, node> &sym_tab)=0;
+public:
+    virtual string evaluate_expression(map<string, node>& sym_tab) = 0;
+    virtual ~string_expression() {}
 };
 
 class concatenate_expr : public string_expression {
-    public:
-        concatenate_expr(string_expression* left, string_expression* right) : l(left), r(right) {}
+public:
+    concatenate_expr(string_expression* left, string_expression* right) : l(left), r(right) {}
 
-        virtual string evaluate_expression(map<string, node>& sym_tab) {
-            return l -> evaluate_expression(sym_tab) + r -> evaluate_expression(sym_tab);
-        }
+    virtual string evaluate_expression(map<string, node>& sym_tab) {
+        return l->evaluate_expression(sym_tab) + r->evaluate_expression(sym_tab);
+    }
 
-    private:
-        string_expression* l;
-        string_expression* r;
+private:
+    string_expression* l;
+    string_expression* r;
 };
 
-class string_constant:public string_expression { //do we need this?
- public:
-  string_constant(string val) {saved_val = val;}
+class string_constant : public string_expression {
+public:
+    string_constant(const string& val) : saved_val(val) {}
 
-  virtual string evaluate_expression(map<string, node> &sym_tab) {
-    return saved_val;
-  }
- private:
-  string saved_val;
+    virtual string evaluate_expression(map<string, node>& sym_tab) {
+        return saved_val;
+    }
+
+private:
+    string saved_val;
 };
 
 class statement {
- public:
-  virtual void evaluate_statement(map<string, node> &sym_tab) =0;
-};
-
-class compound_statement: public statement {
- public:
-  compound_statement(statement *first, compound_statement *rest) {
-    f = first;
-    r = rest;
-  }
-  
-  virtual void evaluate_statement(map<string, node> &sym_tab) {
-    if (f!=NULL) {
-      f->evaluate_statement(sym_tab);
-    }
-    if (r!=NULL) {
-      r->evaluate_statement(sym_tab);
-    }
-  }
- private:
-  compound_statement *r;
-  statement *f;
-};
-
-class build_statement: public statement { //note, this is from a version with binary trees
-	public:
-		build_statement(string_expression *n, integer_expression *w, string_expression *childOf)
- 		{
-			name = n;
-			weight = w;
-			isachildof = childOf;
-		}			
-		virtual void evaluate_statement(map<string, node> &sym_tab)	
-		{	//create the new node
-			node *t = new node;
-			t->name = name->evaluate_expression(sym_tab);
-			t->weight = weight->evaluate_expression(sym_tab);	//changed from w to weight
-      cout << t->name << " and " << t->weight << endl;
-
-			node *parent;	// find the parent
-			map<string, node>::iterator p;
-			p =sym_tab.find(isachildof->evaluate_expression(sym_tab));
-			if (p!=sym_tab.end())
- 			{	parent = &(*p).second;		} //changed from p.second
-      insert(parent, t->name, t->weight, parent->name); //changed from binary tree
-
-			sym_tab.insert(pair<string, node>(t->name, *t)); //changed from t.name to t->name
-		}
-	private:
-		string_expression *name;
-		integer_expression  *weight;
-		string_expression  *isachildof;
-};
-
-class root_statement: public statement { //note, this is from a version with binary trees
 public:
-  root_statement(string_expression *n, integer_expression *w)
-  {
-    name = n;
-    weight = w;
-    isachildof = nullptr;
-  }			
-  virtual void evaluate_statement(map<string, node> &sym_tab)	
-  {	//create the new node
-    node *t = new node;
-    t->name = name->evaluate_expression(sym_tab);
-    t->weight = weight->evaluate_expression(sym_tab);	//changed from w to weight
-    cout << t->name << " and " << t->weight << endl;
-
-    insert(t, "root", t->weight, ""); //changed from binary tree
-    sym_tab.insert(pair<string, node>(t->name, *t)); //changed from t.name to t->name
-  }
-private:
-  string_expression *name;
-  integer_expression  *weight;
-  string_expression  *isachildof;
+    virtual void evaluate_statement(map<string, node>& sym_tab) = 0;
+    virtual ~statement() {}
 };
 
+class compound_statement : public statement {
+public:
+    compound_statement(statement* first, compound_statement* rest) : f(first), r(rest) {}
 
-class int_assignment_statement: public statement {
- public:
-  int_assignment_statement(char *id, integer_expression *rhs) {
-    ident = id;
-    r_side = rhs;
-  }
-  virtual void evaluate_statement(map<string, node> &sym_tab) {
-    
-    int temp = r_side->evaluate_expression(sym_tab);
+    virtual void evaluate_statement(map<string, node>& sym_tab) {
+        if (f) f->evaluate_statement(sym_tab);
+        if (r) r->evaluate_statement(sym_tab);
+    }
 
-    cout << "Assigning" << ident << " to " << temp << endl;
+private:
+    statement* f;
+    compound_statement* r;
+};
 
-    sym_tab[ident].weight=temp; //added .weight
-  }
+class build_statement : public statement {
+public:
+    build_statement(string_expression* n, integer_expression* w, string_expression* childOf)
+        : name(n), weight(w), isachildof(childOf) {}
 
+    virtual void evaluate_statement(map<string, node>& sym_tab) {
+        node t;
+        t.name = name->evaluate_expression(sym_tab);
+        t.weight = weight->evaluate_expression(sym_tab);
+        insert(sym_tab["root"], t.name, t.weight, isachildof->evaluate_expression(sym_tab));
+    }
 
-  private: 
+private:
+    string_expression* name;
+    integer_expression* weight;
+    string_expression* isachildof;
+};
+
+class loop_statement : public statement {
+public:
+    loop_statement(const string& identifier, const pair<int, int>& range, compound_statement* body)
+        : id(identifier), rng(range), stmts(body) {}
+
+    virtual void evaluate_statement(map<string, node>& sym_tab) {
+        for (int i = rng.first; i <= rng.second; ++i) {
+            sym_tab[id] = node(id, i);
+            stmts->evaluate_statement(sym_tab);
+        }
+    }
+
+private:
+    string id;
+    pair<int, int> rng;
+    compound_statement* stmts;
+};
+
+class root_statement : public statement {
+public:
+    root_statement(string_expression* n, integer_expression* w) : name(n), weight(w) {}
+
+    virtual void evaluate_statement(map<string, node>& sym_tab) {
+        node t;
+        t.name = name->evaluate_expression(sym_tab);
+        t.weight = weight->evaluate_expression(sym_tab);
+        insert(sym_tab["root"], t.name, t.weight, "");
+    }
+
+private:
+    string_expression* name;
+    integer_expression* weight;
+};
+
+class int_assignment_statement : public statement {
+public:
+    int_assignment_statement(const string& id, integer_expression* rhs) : ident(id), r_side(rhs) {}
+
+    virtual void evaluate_statement(map<string, node>& sym_tab) {
+        sym_tab[ident].weight = r_side->evaluate_expression(sym_tab);
+    }
+
+private:
     string ident;
-    integer_expression *r_side;
-  };
+    integer_expression* r_side;
+};
 
-class string_assignment_statement: public statement {
- public:
-  string_assignment_statement(char *id, string_expression *rhs) {
-    ident = id;
-    r_side = rhs;
-  }
-  virtual void evaluate_statement(map<string, node> &sym_tab) {
-    
-    string temp = r_side->evaluate_expression(sym_tab);
+class string_assignment_statement : public statement {
+public:
+    string_assignment_statement(const string& id, string_expression* rhs) : ident(id), r_side(rhs) {}
 
-    cout << "Assigning" << ident << " to " << temp << endl;
+    virtual void evaluate_statement(map<string, node>& sym_tab) {
+        sym_tab[ident].name = r_side->evaluate_expression(sym_tab);
+    }
 
-    sym_tab[ident].name=temp; //added .name
-  }
-
-
-  private: 
+private:
     string ident;
-    string_expression *r_side;
-  };
+    string_expression* r_side;
+};
 
+#endif // PARSE_TREE_H
--- tree_builder.y
+++ tree_builder.y
@@ -1,15 +1,16 @@
-// Your YACC program to parse the grammar and perform syntactic analysis
-//*****************************************************************************************
-// Filename: tree_builder.y
-// Authors: Hunter Smith and Johnathan Reilly
-// Brief: A YACC program
-// ****************************************************************************************
-// Includes
 %{
 #include <vector>
 #include <string>
+#include <map>
+#include <iostream>
 #include "parse_tree.h"
 
+using namespace std;
+
+extern int yylex();
+extern void yyerror(const char *);
+
+map<string, node> sym_tab;
 %}
 
 %union {
@@ -20,96 +21,55 @@
   compound_statement *c_ptr;
 }
 
-%{
-extern int yylex();
-extern void yyerror(char *String);  
-
-using namespace std;
-#include <iostream>
-
-%}
-
-%start start_var
 %token <s_val> TKBUILDNODE TKSTRINGLITERAL FOR IN IDENTIFIER TKINTLITERAL
 %token <s_val> TKISCHILDOF TKNAME TKWEIGHT TKID TKINT
-%left '*' '/' '%'
+%left '*' '/'
 %left '+' '-'
 
-//%type <s_val> TKID TKINT TKNAME TKWEIGHT TKISCHILDOF //FOR IN IDENTIFIER
-%type <str_ptr> string_expression //TKSTRINGLITERAL
-%type <int_ptr> integer_expression //TKINTLITERAL
-%type <s_ptr> statement build_statement loop_statement range int_assignment_statement string_assignment_statement
+%type <s_val> string_expression
+%type <int_ptr> integer_expression
+%type <s_ptr> statement build_statement loop_statement range
 %type <c_ptr> prog start_var
 
-//Declarations
+%start start_var
 
 %%
 
-start_var : prog { // At this point, the 
-                   // the program is done --- let's evaluate the
-                   // program
-                   map<string,node> my_sym_tab;
-                   $$= $1;
-                   $1->evaluate_statement(my_sym_tab);
-}
+start_var : prog { $$ = $1; }
 
-prog : statement prog {$$ = new compound_statement($1,$2);}
-        | {$$ = NULL;}
-        ;
+prog : statement prog { $$ = new compound_statement($1, $2); }
+     | { $$ = NULL; };
 
-statement : build_statement {$$ = $1;}
-          | loop_statement {$$ = $1;}
-          | int_assignment_statement {$$ = $1;}
-          | string_assignment_statement {$$ = $1;}
-          ;
+statement : build_statement { $$ = $1; }
+          | loop_statement { $$ = $1; };
 
-int_assignment_statement : TKID '=' integer_expression ';' {$$ = new int_assignment_statement($1,$3);}
+loop_statement : FOR IDENTIFIER IN '[' range ']' '{' prog '}' { $$ = new loop_statement($2, $5, $9); };
 
-string_assignment_statement : TKID '=' string_expression ';' {$$ = new string_assignment_statement($1,$3);}
+range : TKINTLITERAL ':' TKINTLITERAL { $$ = make_pair(atoi($1), atoi($3)); }
+      | TKSTRINGLITERAL ',' range { $$ = $1 + ", " + $3; }
+      | TKSTRINGLITERAL { $$ = $1; };
 
-integer_expression : TKINT {$$ = new int_constant(atoi($1));}
-                | TKID {$$= new variable($1);}
-                | integer_expression '+' integer_expression {$$=new plus_expr($1,$3);}
-                | '(' integer_expression ')' {$$ = $2;} 
-                ;
+build_statement : TKBUILDNODE '{' TKNAME '=' string_expression ';' TKWEIGHT '=' integer_expression ';' TKISCHILDOF '=' string_expression ';' '}' ';' {
+                    $$ = new build_statement($5, $9, $13);
+                    sym_tab[$5->evaluate_expression(sym_tab)] = node($5->evaluate_expression(sym_tab), $9->evaluate_expression(sym_tab));
+                  }
+                | TKBUILDNODE '{' TKNAME '=' string_expression ';' TKWEIGHT '=' integer_expression ';' '}' ';' {
+                    $$ = new root_statement($5, $9);
+                    sym_tab[$5->evaluate_expression(sym_tab)] = node($5->evaluate_expression(sym_tab), $9->evaluate_expression(sym_tab));
+                  };
 
-string_expression : TKID {$$ = new string_constant($1);}
-                | string_expression '+' string_expression {$$ = new concatenate_expr($1,$3);}
-                | '(' string_expression ')' {$$ = $2;}
-                ;
+integer_expression : TKINT { $$ = new int_constant(atoi($1)); }
+                | TKID { $$ = new variable($1); }
+                | integer_expression '+' integer_expression { $$ = new plus_expr($1, $3); }
+                | '(' integer_expression ')' { $$ = $2; };
 
-build_statement : TKBUILDNODE '{' TKNAME '=' string_expression ';' TKWEIGHT '=' integer_expression ';' TKISCHILDOF '=' string_expression ';' '}' ';' {$$ = new build_statement($5, $9, $13);}
-                | TKBUILDNODE '{' TKNAME '=' string_expression ';' TKWEIGHT '=' integer_expression ';' '}' ';' {$$ = new root_statement($5, $9);}
-                ;
-
-/* attribute_list : attribute
-               | attribute_list attribute
-               ; */
-
-/* attribute : TKNAME '=' string_expression ';' {$$ = $3;}
-          | TKWEIGHT '=' integer_expression ';' {$$ = new int_assignment_statement($1,$3);}
-          | TKISCHILDOF '=' string_expression ';' {$$ = new string_assignment_statement($1,$3);}
-          ;  */
-
-loop_statement : FOR IDENTIFIER IN '[' range ']' '{' prog '}' {}
-               ;
-
-range : TKINTLITERAL ':' TKINTLITERAL {}
-      | TKSTRINGLITERAL ',' range {}
-      | TKSTRINGLITERAL {}
-      ;
+string_expression : TKID { $$ = new string_constant($1); }
+                | string_expression '+' string_expression { $$ = new concatenate_expr($1, $3); }
+                | '(' string_expression ')' { $$ = $2; };
 
 %%
-#include "lex.yy.c"
 
-void yyerror(char *error_string)
-{
-  cout << "Error : " << error_string << endl;
-  exit(-1);
-}
-
-// Auxiliary
 int main() {
-    yyparse();
-    return 0;
+  yyparse();
+  return 0;
 }
